312a0c62cd1e39c76f757945ce4e84fe4cafd525
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-04-06 17:10:39 +0300

    2

---
+++ DeviceModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Modbus_simulator
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    class DeviceModel: ObservableObject
+    {
+        private static float ToFourDigits(float value)
+        {
+            int lgth = Math.Truncate(Math.Abs(value)).ToString().Length;
+            return lgth > 4 ? 0 : (float)Math.Round(value, 4 - lgth);
+        }
+        private SolidColorBrush _colorChange;
+        private SolidColorBrush _yellowColor = new SolidColorBrush(Color.FromRgb(255, 216, 0));
+        private SolidColorBrush _greenColor = new SolidColorBrush(Color.FromRgb(6, 176, 37));
+        public SolidColorBrush ColorChange
+        {
+            get { return _colorChange; }
+            set
+            {
+                _colorChange = value;
+                OnPropertyChanged("ColorChange");
+            }
+        }
+        private float _value;
+
+        public float Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        private bool _ready;
+        public bool Ready
+        {
+            get { return _ready; }
+            set { _ready = value;
+                OnPropertyChanged("Ready");
+                }
+        }
+
+        private bool _setpointHold;
+        public bool SetpointHold
+        {
+            get { return _setpointHold; }
+            set { _setpointHold = value;
+                OnPropertyChanged("SetpointHold");
+            }
+        }
+        private float UpdateSetpoint()
+        {
+            return 1;
+        }
+
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Modbus.Data;
+using Modbus.Device;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,33 @@ namespace Modbus_simulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread flow;
+        ModbusSlave slave;
         public MainWindow()
         {
             InitializeComponent();
+            flow = new Thread(SlaveThread);
+            flow.Start();
+        }
+        public void SlaveThread()
+        {
+            using (SerialPort slavePort = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One))
+            {
+                slavePort.Open();
+                byte unitID = 1;
+                slave = ModbusSerialSlave.CreateRtu(unitID, slavePort);
+                slave.DataStore = DataStoreFactory.CreateDefaultDataStore();
+                slave.Listen();
+            }
+        }
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            slave.Dispose();
         }
     }
 }
